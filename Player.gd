1d2a56688da491b58d499737e6953e15db443a37
heliosalesjr <heliosalesjr@gmail.com>
2023-07-26 15:48:47 -0300

    signals and restarting

--- Player.gd
+++ Player.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
+var force = 1000
 
 func _physics_process(delta):
-	var force = 100
+
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force,0))
 	if Input.is_action_pressed("move_left"):
